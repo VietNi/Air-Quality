0ba0cdac69517c16024688ee7d0304c92d18f6be
VietNi <nguyenquocvietxxx@gmail.com>
2023-01-15 14:21:50 +0700

    first commit

---
+++ src/Router/routes.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var MongoClient = require('mongodb').MongoClient;
+const assert = require("assert");
+var router = express.Router();
+const uri = "mongodb+srv://vietnguyen:500anhem@cluster0.6z4y7rp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+let data_raw = {}
+let data_home = {
+        humi: '',
+        temp: '',
+        co2: ''
+}
+router.get("/home", async function (req, res) {
+        const db = client.db("mydb");
+        const collection = db.collection("data_humi");
+        data_raw = await collection.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.humi = JSON.stringify(data_raw).substring(86, 91)
+        const collection1 = db.collection("data_temp");
+        data_raw = await collection1.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.temp = JSON.stringify(data_raw).substring(86, 91)
+        const collection2 = db.collection("data_mq7");
+        data_raw = await collection2.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.co2 = JSON.stringify(data_raw).substring(85, 89)
+        res.render("../views/home", { "data_home": data_home })
+});
+
+router.get("/humi", function (req, res) {
+        const db = client.db("mydb");
+        const collection = db.collection("data_humi");
+        collection.find({}).sort({ Time: -1 }).toArray(function (err, humi_list) {
+                assert.equal(err, null);
+                res.render("../views/humi", { "data_humi": humi_list })
+        });
+});
+
+router.get("/temp", function (req, res) {
+        const db = client.db("mydb");
+        const collection = db.collection("data_temp");
+        collection.find({}).sort({ Time: -1 }).toArray(function (err, temp_list) {
+                assert.equal(err, null);
+                res.render("../views/temp", { "data_temp": temp_list })
+        });
+});
+
+router.get("/gas", function (req, res) {
+        const db = client.db("mydb");
+        const collection = db.collection("data_mq7");
+        collection.find({}).sort({ Time: -1 }).toArray(function (err, mq7_list) {
+                assert.equal(err, null);
+                res.render("../views/gas", { "data_mq7": mq7_list })
+        });
+});
+
+router.post("/home", async function (req, res) {
+        const db = client.db("mydb");
+        const collection = db.collection("data_humi");
+        data_raw = await collection.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.humi = JSON.stringify(data_raw).substring(86, 91)
+        const collection1 = db.collection("data_temp");
+        data_raw = await collection1.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.temp = JSON.stringify(data_raw).substring(86, 91)
+        const collection2 = db.collection("data_mq7");
+        data_raw = await collection2.find({}).sort({ Time: -1 }).limit(1).toArray()
+        data_home.co2 = JSON.stringify(data_raw).substring(85, 89)
+        res.render("../views/home", { "data_home": data_home })
+});
+
+
+module.exports = router;
---
+++ src/index.js
@@ -0,0 +1,128 @@
+var express = require("express");
+var MongoClient = require('mongodb').MongoClient;
+const assert = require("assert");
+var path = require("path");
+//var bodyParser = require("body-parser");
+const uri = "mongodb+srv://vietnguyen:500anhem@cluster0.6z4y7rp.mongodb.net/?retryWrites=true&w=majority";
+var routes = require('./Router/routes.js');
+const { Client } = require("mqtt");
+var app = express();
+
+const client = new MongoClient(uri);
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(routes);
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("connect suscessful");
+  app.listen(app.get("port"), function () {
+    console.log("server started on port " + app.get("port"));
+  });
+});
+
+//IndoorAirQualityMonitoringSystem
+
+// // // http://www.steveio.com/2020/07/11/mqtt-to-websockets-esp32-nodejs-d3/
+// async function connect1() {
+//   MongoClient.connect(uri, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.createCollection("data_mq7", function (err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
+// }
+
+// async function connect2() {
+//   MongoClient.connect(uri, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.createCollection("data_humi", function (err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
+// }
+// async function connect3() {
+//   MongoClient.connect(uri, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.createCollection("data_temp", function (err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
+// }
+
+// connect1();
+// connect2();
+// connect3();
+
+const mqtt = require("mqtt");
+
+const host = "broker.emqx.io";
+const port = "1883";
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+
+const connectUrl = `mqtt://${host}:${port}`;
+const client_mqtt = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: "emqx",
+  password: "public",
+  reconnectPeriod: 1000,
+});
+
+const topic_mq7 = "esp32_pub_mq7";
+const topic_humi = "esp32_pub_humi";
+const topic_temp = "esp32_pub_temp";
+client_mqtt.on("connect", () => {
+  console.log("Connected");
+  client_mqtt.subscribe([topic_mq7], () => {
+    console.log(`Subscribe to topic '${topic_mq7}'`);
+  });
+  client_mqtt.subscribe([topic_humi], () => {
+    console.log(`Subscribe to topic '${topic_humi}'`);
+  });
+  client_mqtt.subscribe([topic_temp], () => {
+    console.log(`Subscribe to topic '${topic_temp}'`);
+  });
+});
+
+client_mqtt.on("message", (topic, payload) => {
+  MongoClient.connect(uri, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    switch (topic) {
+      case topic_mq7:
+        var myobj = { Device: 'esp32', topic: topic_mq7, Value: payload.toString(), Unit: "ppm", Time: new Date() };
+        dbo.collection("data_mq7").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          db.close();
+        });
+        break;
+      case topic_humi:
+        var myobj = { Device: 'esp32', topic: topic_humi, Value: payload.toString(), Unit: "%", Time: new Date() };
+        dbo.collection("data_humi").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          db.close();
+        });
+        break;
+      case topic_temp:
+        var myobj = { Device: 'esp32', topic: topic_temp, Value: payload.toString(), Unit: "°C", Time: new Date() };
+        dbo.collection("data_temp").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          db.close();
+        });
+        break;
+    }
+  });
+});
